f92bc5353a5816a1b5965e25d67089b16351d058
Charles-Dang <dangcharles10@gmail.com>
2025-09-28 01:23:03 -0400

    Added outfit feature, saved outfit feature, images

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def home():
 def folder_page():
     return render_template("folder.html")
 
+@app.route("/outfits/create")
+def outfits_create():
+    return render_template("create.html")
+
+@app.route("/outfits/saved")
+def outfits_saved():
+    return render_template("saved.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
